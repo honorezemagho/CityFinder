cdaca2688f42398e0a718a7921b47b771198797d
seven academy <sevenacademy@sevens-iMac.local>
2020-07-03 16:55:41 +0100

    adding ajax and reverse geodecoging method to display current town

--- assets/js/getLocation.js
+++ assets/js/getLocation.js
@@ -2,12 +2,17 @@ let x = document.getElementById("demo");
 
 function success(position) {
   let coordinates = position.coords;
- 
+  const url = 'https://api.bigdatacloud.net/data/reverse-geocode-client?latitude='+coordinates.latitude+'&longitude='+ coordinates.longitude+'&localityLanguage=en';
+
+  $.get(url, function(data, status){
+    console.log("\nStatus: " + status);
    x.innerHTML = `<h4> Your Current Positon is:  </h4><br> 
 <h4>  Latitude : <span>   ${coordinates.latitude}</span> </h4> <br> 
 <h4> Longitude: <span>   ${coordinates.longitude}  </span> </h4> <br> 
 <h4> More or less <span>  ${coordinates.accuracy} </span> meters. </h4> <br>
+<h4> You are Actually in  <span>  ${data.locality} </span> 
   `;
+  });
 }
 
 function error(err) {
